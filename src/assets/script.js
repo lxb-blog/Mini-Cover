307c15569131a7c16fc64a9e59098a4dc940d6de
lxb-blog <97530887+lxb-blog@users.noreply.github.com>
2024-10-24 21:11:22 +0800

    Update script.js

--- src/assets/script.js
+++ src/assets/script.js
@@ -13,7 +13,7 @@ export const state = {
     watermark: "@梦爱吃鱼",
     textSize: 200,
     squareSize: 300,
-    text: '梦爱吃鱼',
+    text: '李小白',
     bgBlur: 3,
     iconBackground: false,
     iconBorder: false,
